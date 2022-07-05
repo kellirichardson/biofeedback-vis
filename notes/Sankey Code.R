b7206dfb6125b49526b2a732e06ceb7aa3d57160
kellirichardson <108764199+kellirichardson@users.noreply.github.com>
2022-07-05 15:12:56 -0700

    Add files via upload

---
+++ notes/Sankey Code.R
@@ -0,0 +1,68 @@
+#Kelli Richardson 
+#July 5, 2022
+#Sankey Code 
+
+#Comprehensive Sankey 
+library(ggplot2)
+library(ggsankey)
+library(dplyr)
+library(networkD3)
+attach(Sankey_data)
+Sankey <- Sankey_data %>%
+  rename(Biomeasures = `Biological measures`) %>% 
+  separate_rows(Communication, sep = ",", convert = TRUE) %>% 
+  separate_rows(Biomeasures, sep = ",", convert = TRUE) %>% 
+  separate_rows(Behaviors, sep = ",", convert = TRUE) %>%
+  separate_rows(Collection, sep = ",", convert = TRUE) %>%
+  separate_rows('Frequency of feedback', sep = ",", convert = TRUE) %>%
+  make_long(Domain,Biomeasures,Collection, 'Frequency of feedback', Communication, Behaviors, Outcome)
+head(Sankey)
+
+test_sankey <- ggplot(Sankey, aes(x = x, 
+                                  next_x = next_x, 
+                                  node = node, 
+                                  next_node = next_node,
+                                  fill = factor(node),
+                                  label = node)) +
+  geom_sankey(flow.alpha = 0.5) +
+  geom_sankey_label(size = 3.5, fill = "white") +
+  theme_sankey(base_size = 16) + 
+  theme(legend.position = "none") + 
+  xlab(NULL)
+test_sankey
+
+#Notes about the above Sankey 
+#The "make_long" function is the main function. This is where you list all the "columns" that are in the Sankey. The order matters here. 
+#Regardless of what you change in lines 11-19, there is no need to change lines 21-32. Just run them again after making changes. 
+#The "separate_rows" function is used because in some rows in the data, for instance, there are multiple biomeasures
+
+
+#How to filter 
+#Note - the only change from above is on line 49 
+library(ggplot2)
+library(ggsankey)
+library(dplyr)
+library(networkD3)
+attach(Sankey_data)
+Sankey <- Sankey_data %>%
+  rename(Biomeasures = `Biological measures`) %>% 
+  filter(Biomeasures == "Glucose") %>% 
+  separate_rows(Communication, sep = ",", convert = TRUE) %>% 
+  separate_rows(Behaviors, sep = ",", convert = TRUE) %>%
+  separate_rows(Collection, sep = ",", convert = TRUE) %>%
+  separate_rows('Frequency of feedback', sep = ",", convert = TRUE) %>%
+  make_long(Domain,Biomeasures,Collection, 'Frequency of feedback', Communication, Behaviors, Outcome)
+head(Sankey)
+
+test_sankey <- ggplot(Sankey, aes(x = x, 
+                                  next_x = next_x, 
+                                  node = node, 
+                                  next_node = next_node,
+                                  fill = factor(node),
+                                  label = node)) +
+  geom_sankey(flow.alpha = 0.5) +
+  geom_sankey_label(size = 3.5, fill = "white") +
+  theme_sankey(base_size = 16) + 
+  theme(legend.position = "none") + 
+  xlab(NULL)
+test_sankey
