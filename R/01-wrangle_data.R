a195ff6bac9e493be4898fe74e13baf023254f76
Michelle Jospe <michellejospe@gmail.com>
2022-08-05 15:42:05 +1200

    Experimented with forcats - it has potential but got confused with the totals in the table. Also got stuck trying to rename stuff.

--- R/01-wrangle_data.R
+++ R/01-wrangle_data.R
@@ -2,6 +2,7 @@
 
 library(tidyverse) 
 library(janitor)
+library(forcats)
 
 # Read in data ------------------------------------------------------------
 articles_raw <- read_csv("data_raw/Scoping.csv")
@@ -22,6 +23,7 @@ articles <-
   separate_rows(behavior, sep = ",(?!\\s)") %>% 
   separate_rows(collection, sep = ",(?!\\s)") %>%
   separate_rows(frequency, sep = ",(?!\\s)") %>%
+  separate_rows(domain, sep = ",(?!\\s)") %>% 
   
   # Now replace anything that starts with "Other : " with "Other" to lump
   # categories (the "^" is another bit of regex that just says this has to be at
@@ -30,5 +32,13 @@ articles <-
     across(c(biomarker, behavior), 
            ~if_else(str_detect(., "^Other : "), "Other", .))
   )
+  
+articles$domain <- rename (articles$domain, 'Neurodegenerative disease' = 'Alzheimer’s disease') # not working. 
+# need to also rename obstructive sleep apnea as sleep apnea
+
+#Forcats experiment (I have no idea whether this goes here)
+articles$domain_lumped <- fct_lump_min(articles$domain, 5 , other_level = "Other") #not sure if 5 is appropriate, just experimenting
+table (articles$domain_lumped) #now I'm confused since it's listing more articles per Diabetes domain than in our entire database!
+#ideally we would want to also do this for outcome, and behavior 
 
 write_csv(articles, "app/articles_clean.csv")
